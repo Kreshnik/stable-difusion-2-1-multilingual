4636d0d2879699e6398c7dc3951bff957934b18b
Kreshnik Hasanaj <kreshnik.hasanaj@gmail.com>
2022-12-16 22:45:57 +0100

    refactor: Don't translate English to English

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 from googletrans import Translator
 from InquirerPy import inquirer
 
-translator = Translator()
-
 language_code = inquirer.text(message="What is your language code?").execute()
 description = inquirer.text(message="Describe the image in your language:").execute()
 
-translation = translator.translate(description, src=language_code)
+if language_code != "en":
+    translator = Translator()
+    translation = translator.translate(description, src=language_code)
+    text = translation.text
+else:
+    text = description
 
 model_id = "stabilityai/stable-diffusion-2-1"
 
@@ -20,6 +23,6 @@ pipe.enable_attention_slicing()
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
-images = pipe(translation.text).images
+images = pipe(text).images
 images[0].save(os.path.join('images', slugify(description) + ".png"))
 images[0].show()
